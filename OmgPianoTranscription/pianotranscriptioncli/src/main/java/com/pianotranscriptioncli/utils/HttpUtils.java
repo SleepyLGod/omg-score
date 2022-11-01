4183d4061ca4bd20a8e202dbb56ae68215e1aadc
odin <2441164168lhd@gmail.com>
2022-11-02 03:14:47 +0800

    utils updated

---
+++ OmgPianoTranscription/pianotranscriptioncli/src/main/java/com/pianotranscriptioncli/utils/HttpUtils.java
@@ -0,0 +1,31 @@
+package com.pianotranscriptioncli.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class HttpUtils {
+
+    /**
+     * Get the IP address of client machine.
+     * @param request
+     * @return IP address
+     */
+    public static String getIpAddr(HttpServletRequest request) {
+        String ipAddress = request.getHeader("x-forwarded-for");
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+        }
+        // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+        if (ipAddress != null && ipAddress.length() > 15) {
+            if (ipAddress.indexOf(",") > 0) {
+                ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
+            }
+        }
+        return ipAddress;
+    }
+}
